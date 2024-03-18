881b0c07ebb821250904eb8f0b633f5a237ffb2f
VictorRR-dev <53363935+VictorRiosIK@users.noreply.github.com>
2024-03-17 22:38:52 -0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 axios.defaults.withCredentials = true;
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('proyecto-tt-api.vercel.app/register', {name, email, password})
+    axios.post('https://proyecto-tt-api.vercel.app/register', {name, email, password})
     .then(result => console.log(result))
     .catch(err => console.log(err))
   }
