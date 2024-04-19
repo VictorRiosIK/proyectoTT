124d88e09f0324c831abacfbb398d7c36efbb858
VictorRR-dev <53363935+VictorRiosIK@users.noreply.github.com>
2024-04-19 10:16:01 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,108 +19,87 @@ app.get("/", (req, res) => {
     res.json("Hello");
 })
 app.post('/register', (req, res) => {
-   const {name, email, password} = req.body;
-    RegisterModel.findOne({email: email})
-    .then(user => {
-        if(user) {
-            const token = jwt.sign({ email: email }, 'q66eSaeLDeYHOdZBW5LeWi2yejcdirPxliq3Lf+mLdo');
-                        res.json({ user: "Ya existe una cuenta", token: token });
-        } else {
-            RegisterModel.create({name: name, email: email, password: password})
-            .then(result => {
-                // Generar un token JWT
-                        const token = jwt.sign({ email: email }, 'tu_secreto');
+    const { name, email, password } = req.body;
+    RegisterModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                res.json({ error: 'Ya existe una cuenta registrada con este correo electrónico.' });
+            } else {
+                RegisterModel.create({ name: name, email: email, password: password })
+                    .then(result => {
+                        const token = jwt.sign({ email: email }, 'q66eSaeLDeYHOdZBW5LeWi2yejcdirPxliq3Lf+mLdo');
                         res.json({ user: result, token: token });
-            })
-            .catch(err => res.json(err))
-        }
-    }).catch(err => res.json(err))
-    
-   
+                    })
+                    .catch(err => res.status(500).json({ error: 'Error al crear la cuenta.' }));
+            }
+        })
+        .catch(err => res.status(500).json({ error: 'Error interno del servidor.' }));
 });
-app.post('/registerProfessional', (req, res) => {
-    const { name, email, password,rol } = req.body;
 
-    // Verificar si el correo electrónico ya existe en RegisterModel
+app.post('/registerProfessional', (req, res) => {
+    const { name, email, password, rol } = req.body;
     RegisterModel.findOne({ email: email })
         .then(existingUser => {
             if (existingUser) {
-                // Si el correo electrónico ya existe, devolver un mensaje de error
-                return res.status(400).json({ error: 'El correo electrónico ya está registrado' });
+                return res.status(400).json({ error: 'El correo electrónico ya está registrado.' });
             } else {
-                // Si el correo electrónico no existe, cifrar la contraseña
                 const hashedPassword = bcrypt.hashSync(password, 10);
-
-                // Buscar si el correo electrónico ya existe en RegisterProfessionalModel
                 RegisterProfessionalModel.findOne({ email: email })
                     .then(professional => {
                         if (professional) {
-                            // Si el correo electrónico ya existe en RegisterProfessionalModel, devolver un mensaje de error
-                            return res.status(400).json({ error: 'Ya existe una cuenta de profesional' });
+                            return res.status(400).json({ error: 'Ya existe una cuenta de profesional.' });
                         } else {
-                            // Si el correo electrónico no existe en RegisterProfessionalModel, crear un nuevo usuario profesional
-                            RegisterProfessionalModel.create({ name: name, email: email, password: hashedPassword,rol:rol })
+                            RegisterProfessionalModel.create({ name: name, email: email, password: hashedPassword, rol: rol })
                                 .then(result => {
-                                    // Generar un token JWT
                                     const token = jwt.sign({ email: email }, 'q66eSaeLDeYHOdZBW5LeWi2yejcdirPxliq3Lf+mLdo');
                                     res.json({ professional: result, token: token });
                                 })
-                                .catch(err => res.status(500).json({ error: 'Error al crear el usuario profesional' }));
+                                .catch(err => res.status(500).json({ error: 'Error al crear la cuenta profesional.' }));
                         }
                     })
-                    .catch(err => res.status(500).json({ error: 'Error al buscar el usuario profesional' }));
+                    .catch(err => res.status(500).json({ error: 'Error al buscar la cuenta profesional.' }));
             }
         })
-        .catch(err => res.status(500).json({ error: 'Error al buscar el usuario' }));
+        .catch(err => res.status(500).json({ error: 'Error al buscar la cuenta.' }));
 });
+
 app.post('/registerStudent', (req, res) => {
     const { name, email, password, boleta, rol } = req.body;
-
-    // Hash de la contraseña
-     const hashedPassword = bcrypt.hashSync(password, 10);
-
-        RegisterStudentModel.findOne({ email: email })
-            .then(user => {
-                if (user) {
-                    return res.json({ user: "Ya existe una cuenta", token: token });
-                } else {
-                    // Crear un nuevo usuario con la contraseña cifrada
-                    RegisterStudentModel.create({ name: name, email: email, password: hashedPassword, boleta: boleta, rol: rol })
-                        .then(result => {
-                            // Generar un token JWT
-                            const token = jwt.sign({ email: email }, 'q66eSaeLDeYHOdZBW5LeWi2yejcdirPxliq3Lf+mLdo');
-                            res.json({ user: result, token: token });
-                        })
-                        .catch(err => res.status(500).json({ error: 'Error al crear el usuario' }));
-                }
-            })
-            .catch(err => res.status(500).json({ error: 'Error al buscar el usuario' }));
-   
-});
-// Endpoint para inicio de sesión
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-
+    const hashedPassword = bcrypt.hashSync(password, 10);
     RegisterStudentModel.findOne({ email: email })
         .then(user => {
             if (user) {
-                // Si el usuario existe, comparar la contraseña almacenada con la proporcionada
-                bcrypt.compare(password, user.password, function(err, result) {
+                return res.status(400).json({ error: 'Ya existe una cuenta registrada con este correo electrónico.' });
+            } else {
+                RegisterStudentModel.create({ name: name, email: email, password: hashedPassword, boleta: boleta, rol: rol })
+                    .then(result => {
+                        const token = jwt.sign({ email: email }, 'q66eSaeLDeYHOdZBW5LeWi2yejcdirPxliq3Lf+mLdo');
+                        res.json({ user: result, token: token });
+                    })
+                    .catch(err => res.status(500).json({ error: 'Error al crear la cuenta de estudiante.' }));
+            }
+        })
+        .catch(err => res.status(500).json({ error: 'Error al buscar la cuenta.' }));
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    RegisterStudentModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                bcrypt.compare(password, user.password, function (err, result) {
                     if (result) {
-                        // Si la contraseña es correcta, generar un token JWT
                         const token = jwt.sign({ email: email }, 'q66eSaeLDeYHOdZBW5LeWi2yejcdirPxliq3Lf+mLdo');
                         res.json({ message: "Inicio de sesión exitoso", token: token });
                     } else {
-                        // Contraseña incorrecta
-                        res.status(401).json({ message: "Credenciales inválidas" });
+                        res.status(401).json({ error: "Credenciales inválidas" });
                     }
                 });
             } else {
-                // Usuario no encontrado
-                res.status(404).json({ message: "Usuario no encontrado" });
+                res.status(404).json({ error: "Usuario no encontrado" });
             }
         })
-        .catch(err => res.status(500).json({ message: "Error interno del servidor" }));
+        .catch(err => res.status(500).json({ error: "Error interno del servidor" }));
 });
 
 app.listen(3001, () => {
