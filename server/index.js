d80f7b46cda86fd3a422c18a8ef39535d6b04834
VictorRR-dev <53363935+VictorRiosIK@users.noreply.github.com>
2024-04-19 14:39:09 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -43,10 +43,10 @@ app.post('/register', (req, res) => {
 
 app.post('/registerProfessional', (req, res) => {
     const { name, email, password, rol } = req.body;
-    RegisterModel.findOne({ email: email })
+    RegisterStudentModel.findOne({ email: email })
         .then(existingUser => {
             if (existingUser) {
-                return res.status(400).json({ error: 'El correo electrónico ya está registrado.' });
+                return res.status(400).json({ error: 'El correo electrónico ya está registrado (Estudiante).' });
             } else {
                 const hashedPassword = bcrypt.hashSync(password, 10);
                 RegisterProfessionalModel.findOne({ email: email })
@@ -70,21 +70,36 @@ app.post('/registerProfessional', (req, res) => {
 
 app.post('/registerStudent', (req, res) => {
     const { name, email, password, boleta, rol } = req.body;
-    const hashedPassword = bcrypt.hashSync(password, 10);
-    RegisterStudentModel.findOne({ email: email })
-        .then(user => {
-            if (user) {
-                return res.status(400).json({ error: 'Ya existe una cuenta registrada con este correo electrónico.' });
+    
+
+    // Verificar si el correo electrónico ya existe en RegisterProfessionalModel
+    RegisterProfessionalModel.findOne({ email: email })
+        .then(professional => {
+            if (professional) {
+                // Si el correo electrónico ya existe en RegisterProfessionalModel, devolver un mensaje de error
+                return res.status(400).json({ error: 'Ya existe una cuenta registrada con este correo electrónico (profesional).' });
             } else {
-                RegisterStudentModel.create({ name: name, email: email, password: hashedPassword, boleta: boleta, rol: rol })
-                    .then(result => {
-                        const token = jwt.sign({ email: email }, jwtSecret);
-                        res.json({ user: result, token: token });
+                // Si el correo electrónico no existe en RegisterProfessionalModel, verificar si existe en RegisterStudentModel
+                RegisterStudentModel.findOne({ email: email })
+                    .then(student => {
+                        if (student) {
+                            // Si el correo electrónico ya existe en RegisterStudentModel, devolver un mensaje de error
+                            return res.status(400).json({ error: 'Ya existe una cuenta registrada con este correo electrónico (estudiante).' });
+                        } else {
+                            const hashedPassword = bcrypt.hashSync(password, 10);
+                            // Si el correo electrónico no existe en ninguno de los dos modelos, crear el registro en RegisterStudentModel
+                            RegisterStudentModel.create({ name: name, email: email, password: hashedPassword, boleta: boleta, rol: rol })
+                                .then(result => {
+                                    const token = jwt.sign({ email: email }, jwtSecret);
+                                    res.json({ user: result, token: token });
+                                })
+                                .catch(err => res.status(500).json({ error: 'Error al crear la cuenta de estudiante.' }));
+                        }
                     })
-                    .catch(err => res.status(500).json({ error: 'Error al crear la cuenta de estudiante.' }));
+                    .catch(err => res.status(500).json({ error: 'Error al buscar la cuenta de estudiante.' }));
             }
         })
-        .catch(err => res.status(500).json({ error: 'Error al buscar la cuenta.' }));
+        .catch(err => res.status(500).json({ error: 'Error al buscar la cuenta de profesional.' }));
 });
 
 app.post('/login', (req, res) => {
