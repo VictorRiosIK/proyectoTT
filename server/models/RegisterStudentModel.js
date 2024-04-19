4f4568f508d37a34072d200d03f5e02d6adb538d
VictorRR-dev <53363935+VictorRiosIK@users.noreply.github.com>
2024-04-18 21:47:13 -0600

    Update RegisterStudentModel.js

--- server/models/RegisterStudentModel.js
+++ server/models/RegisterStudentModel.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose')
 
+const RegisterSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    boleta: String,
+    rol:String
+})
+
+const RegisterStudentModel = mongoose.model("users2", RegisterSchema);
+module.exports = RegisterStudentModel;
