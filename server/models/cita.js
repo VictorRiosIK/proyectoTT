1a51c2ac234e363f29b1c810fd94fdb8d948f871
VictorRR-dev <53363935+VictorRiosIK@users.noreply.github.com>
2024-04-25 19:58:55 -0600

    Update cita.js

--- server/models/cita.js
+++ server/models/cita.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const RegisterSchema = new mongoose.Schema({
     fecha: String,
-    primerHorario: String,
+    09:00-10:30: String,
     segundoHorario: String,
     tercerHorario: String,
     cuartoHorario: String,
