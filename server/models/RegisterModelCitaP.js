edd13799f7d9f0eb2d9ff977bcd3386168db2676
VictorRR-dev <53363935+VictorRiosIK@users.noreply.github.com>
2024-04-30 13:08:06 -0600

    Update RegisterModelCitaP.js

--- server/models/RegisterModelCitaP.js
+++ server/models/RegisterModelCitaP.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose')
 
+const RegisterSchema = new mongoose.Schema({
+    fecha: String,
+    primerHorario: String,
+    segundoHorario: String,
+    tercerHorario: String,
+    cuartoHorario: String,
+    quintoHorario: String,
+    sextoHorario: String
+})
+
+const RegisterModelCitaP = mongoose.model("citaP", RegisterSchema);
+module.exports = RegisterModelCitaP;
