8959e88269caa153c44e88654280089bf5c1967e
VictorRR-dev <53363935+VictorRiosIK@users.noreply.github.com>
2024-05-01 14:45:27 -0600

    Update RegisterCuestionary.js

--- server/models/RegisterCuestionary.js
+++ server/models/RegisterCuestionary.js
@@ -1,9 +1,12 @@
-// Esquema del modelo para guardar las respuestas, incluyendo el correo del usuario
-const RespuestasSchema = new mongoose.Schema({
-  emailUsuario: String, // Correo del usuario
+const mongoose = require('mongoose')
+const RegisterSchema = new mongoose.Schema({
+    emailUsuario: String, // Correo del usuario
   respuesta1: String,
   respuesta2: String,
   respuesta3: String,
   respuesta4: String,
   respuesta5: String
-});
+})
+
+const RegisterCuestionary = mongoose.model("respcuestionary", RegisterSchema);
+module.exports = RegisterCuestionary;
