46813a9d23cd7e535a2f851e8b96efd874b848bd
VictorRR-dev <53363935+VictorRiosIK@users.noreply.github.com>
2024-05-04 14:15:28 -0600

    Update appointmentFollowModel.js

--- server/models/appointmentFollowModel.js
+++ server/models/appointmentFollowModel.js
@@ -3,7 +3,8 @@ const RegisterSchema = new mongoose.Schema({
   email: String, // Correo del usuario
   comentario: String,
   fechaCita:String,
-  horarioCita:String
+  horarioCita:String,
+  tipo:String
 })
 
 const followStudent = mongoose.model("followStudent", RegisterSchema);
