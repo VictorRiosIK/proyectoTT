0493fb9e721b54a1807bd832b3721941c4ca8bf1
VictorRR-dev <53363935+VictorRiosIK@users.noreply.github.com>
2024-05-10 12:56:33 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -992,6 +992,7 @@ app.post('/enviarNotificacion', (req, res) => {
       title: "Título de la notificación",
       body: mensaje,
     },
+    token: "", 
     // Otras opciones como datos adicionales...
     // Aquí se agrega el campo 'scheduledTime' para programar el envío de la notificación
     scheduledTime: horaFormateada.toDate()
