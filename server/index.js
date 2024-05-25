0928733dc54c0a16aa43c467097f78ee774bf9c0
VictorRR-dev <53363935+VictorRiosIK@users.noreply.github.com>
2024-05-24 23:05:53 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const nodemailer = require('nodemailer');
+const crypto = require('crypto');
 const cors = require('cors')
 const RegisterModel = require('./models/Register')
 const RegisterStudentModel=require('./models/RegisterStudentModel');
